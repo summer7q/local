c344a5fef9ee31a8f67aafbccf633492c7a91e25
summer7q <summer7q@163.com>
2017-08-17 22:38:56 +0800

    PY helloworld
    初学python

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import sys;
+print('happy')
+print "welcome to python!"
+print "你好！世界！"
+if True:
+    print "True"
+else:
+    print "False"
+days = ['Monday', 'Tuesday',
+        'Friday']
+print 'word',
+print "hehe"
+sys.stdout.write('std out\n')
+print '''这是啥？
+我也不知道'''
+'''
+据说这是注释
+'''
+raw_input("\n\nPress the enter key to exit.")
